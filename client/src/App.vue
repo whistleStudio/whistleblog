d91c0075722a8181b805bb088bb3aec5fe5fb952
whistleStudio <whistleStudio@126.com>
2022-09-22 17:29:05 +0800

    09221728

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="index">
+    <div class="logo"></div>
   </div>
-  <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style scoped lang="scss">
+  .index {
+    width: 100vw;
+    height: 100vh;
+    background-color: $mainColor;
+    .logo {
+      width: 200px;
+      height: 200px;
+      background: url("wove.svg");
+      opacity: 0.2;
+    }
+  }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
