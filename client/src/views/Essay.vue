bfaa7774984bdddca35ddc0a545be60801fb58ed
whistleStudio <whistleStudio@126.com>
2022-10-11 17:27:33 +0800

    10111727

--- client/src/views/Essay.vue
+++ client/src/views/Essay.vue
@@ -7,6 +7,19 @@
         <input ref="kwIp" type="text" placeholder="关键词">
       </div>
       <div class="main">
+        <div class="cate flex-center">
+          <div>
+            <span>Vue</span>
+            <span class="cate-icon" :class="{'cate-icon-exp': isCateExp}" @click="cateIconClick(1)">&nbsp;{{isCateExp ? '|' : '➡'}}</span>
+          </div>
+          <ul v-if="isCateExp" class="flex-center">
+            <li>JavaScrpit</li>
+            <li>NodeJs</li>
+            <li>TypeScript</li>
+            <li>Vue</li>
+            <li class="cate-icon" @click="cateIconClick(0)">⬅</li>
+          </ul>
+        </div>
         <ul>
           <li v-for="(v, i) in 10"></li>
         </ul>
@@ -20,9 +33,15 @@
   import router from '@/router';
   import { onMounted, ref} from "vue"
   let kwIp = ref<any>(null)
+  let isCateExp = ref<boolean>(false)
   /* 回到主页 */  
   function logoClick () {
     router.push("/")
+  }
+  /* 目录展开/折叠 */
+  function cateIconClick (exp: number) {
+    if (exp) isCateExp.value = true
+    else isCateExp.value = false
   }
   /* ---------------------- */
   onMounted(() => {
@@ -75,18 +94,50 @@
       .main {
         width: 100%;
         height: 600px;
-        background-color: rgb(153, 153, 95);
+        // background-color: rgb(153, 153, 95);
+        .cate {
+          float: left;
+          font: 20px/40px $fontF;
+          >div:first-of-type {
+            span {
+              font-weight: bold;
+              cursor: pointer;
+              &:not(:last-of-type) {opacity: 0.85;}
+            }
+          }
+          .cate-icon {
+            font-size: 23px;
+            opacity: 0.7;
+            cursor: pointer;
+            &:hover {opacity: 1;}
+          }
+          .cate-icon-exp {
+            opacity: 0.85 !important;
+            cursor: default !important;
+          }
+          >ul {
+            li:not(:last-of-type) {
+              opacity: 0.5;
+              margin: 0 35px;
+              cursor: pointer;
+              &:hover {opacity: 0.9;}
+            }
+          }
+        }
         >ul {
           width: 100%;
           display: flex;
           flex-wrap: wrap;
           >li {
-            width: 32%;
+            $width: 32.9%;
+            width: $width;
             height: 150px;
-            background-color: antiquewhite;
+            background-color: rgb(245,245,245);
             // border: 1px solid black;
-            margin-left: 1%;
-            margin-bottom: 1%;
+            margin-bottom: calc((100% - $width * 3) / 2);
+            &:not(:nth-of-type(3n)) {
+              margin-right: calc((100% - $width * 3) / 2);
+            }
           }
         }
       }  
